198b3a5df704f4f9b170d6951d86b5d99b311da1
knight-18 <sameershrivastava46@gmail.com>
2020-03-20 13:26:19 +0530

    initial navbar

---
+++ nav-bar/js.js
@@ -0,0 +1,59 @@
+/* Open when someone clicks on the span element */
+function openNav1() {
+  document.getElementById("myNav1").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav1() {
+  document.getElementById("myNav1").style.width = "0%";
+}
+
+function openNav2() {
+  document.getElementById("myNav2").style.width = "100%";
+}
+
+
+function closeNav2() {
+  document.getElementById("myNav2").style.width = "0%";
+}
+function openNav3() {
+  document.getElementById("myNav3").style.width = "100%";
+}
+
+
+function closeNav3() {
+  document.getElementById("myNav3").style.width = "0%";
+}
+function openNav4() {
+  document.getElementById("myNav4").style.width = "100%";
+}
+
+
+function closeNav4() {
+  document.getElementById("myNav4").style.width = "0%";
+}
+function openNav5() {
+  document.getElementById("myNav5").style.width = "100%";
+}
+
+
+function closeNav5() {
+  document.getElementById("myNav5").style.width = "0%";
+}
+function openNav6() {
+  document.getElementById("myNav6").style.width = "100%";
+}
+
+
+function closeNav6() {
+  document.getElementById("myNav6").style.width = "0%";
+}
+function openNav7() {
+  document.getElementById("myNav7").style.width = "100%";
+}
+
+
+function closeNav7() {
+  document.getElementById("myNav7").style.width = "0%";
+}
+
